e273df6a42a7652a7505f88a7e4e7a879b79b013
Bulat Khayrutdinov <bulatxajrutdinoff@yandex.ru>
2019-11-16 14:09:41 +0300

    CRUD for Product Model

--- app/models/Product.php
+++ app/models/Product.php
@@ -1,15 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hairutdinovbr
- * Date: 2019-11-16
- * Time: 1:30 PM
- */
-
 namespace app\models;
+use RedBeanPHP\R;
 
-
-class Product
+class Product extends AppModel
 {
+  public function __construct()
+  {
+    \core\Db::instance();
+  }
+
+  public function create($name, $description)
+  {
+    $name = $this->safe($name);
+    $description = $this->safe($description);
+
+    $product = R::dispense( 'product' );
+    $product->name = $name;
+    $product->description = $description;
+
+    return R::store($product);
+  }
+
+  public function read($id)
+  {
+    $id = (int)$id;
+    return R::load('product', $id);
+  }
+
+  public function update($id, $name, $description)
+  {
+    $name = $this->safe($name);
+    $description = $this->safe($description);
+
+    $product = $this->read($id);
+    $product->name = $name;
+    $product->description = $description;
+
+    return R::store($product);
+  }
+
+  public function delete($id)
+  {
+    $product = $this->read($id);
+    return R::trash($product);
+  }
 
 }
--- index.php
+++ index.php
@@ -1,3 +1,2 @@
 <?php
 require_once 'config/init.php';
-\core\Db::instance();
