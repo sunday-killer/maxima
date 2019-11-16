e3369afb9619d134045da0917c1049bcbb45bd43
Bulat Khayrutdinov <bulatxajrutdinoff@yandex.ru>
2019-11-16 15:26:16 +0300

    Added some methods: Read, GetList And Update to ProductDetails Model

--- app/models/ProductDetail.php
+++ app/models/ProductDetail.php
@@ -30,4 +30,56 @@ class ProductDetail extends AppModel
     return R::store($productDetail);
   }
 
+  public function read($id)
+  {
+    $id = (int)$id;
+    return R::getAssoc('
+    SELECT
+    pd.id, pd.product_id, p.name, p.description, pd.price, pd.details
+    FROM `productdetail` pd
+    INNER JOIN `product` p
+    ON pd.product_id = p.id
+    WHERE pd.id = ?
+    ', [$id]);
+  }
+
+  public function getList()
+  {
+    return R::getAssoc('
+    SELECT
+    pd.id, pd.product_id, p.name, p.description, pd.price, pd.details
+    FROM `productdetail` pd
+    INNER JOIN `product` p
+    ON pd.product_id = p.id
+    ORDER BY pd.id DESC
+    ');
+  }
+
+  public function update($id, $name, $price, $description, $details)
+  {
+    $name = $this->safe($name);
+    $price = (int)($price);
+    $description = $this->safe($description);
+    $details = $this->safe($details);
+
+    $productDetail = R::load('productdetail', (int)$id);
+
+    $instance_of_product = new Product();
+    $product = $instance_of_product->findByName($name);
+
+    if (is_null($product)) {
+      $productId = $instance_of_product->create($name, $description);
+      $productDetail->product_id = $productId;
+    } else {
+      $productDetail->product_id = $product->id;
+      $product->description = $description;
+      $productUpdate = R::store($product);
+    }
+
+    $productDetail->price = $price;
+    $productDetail->details = $details;
+
+    return R::store($productDetail);
+  }
+
 }
