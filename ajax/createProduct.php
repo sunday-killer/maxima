de858fa7fb07cfb043a3375166d93d73347fc4cf
Bulat Khayrutdinov <bulatxajrutdinoff@yandex.ru>
2019-11-17 21:33:44 +0300

    Validation "Creating Product" form on Backend

--- ajax/createProduct.php
+++ ajax/createProduct.php
@@ -2,6 +2,26 @@
 require_once "../config/init.php";
 if ($_POST) {
   $productDetail = new \app\models\ProductDetail();
-  echo json_encode($productDetail->create($_POST["name"], $_POST["price"], $_POST["description"], $_POST["details"]));
+
+  $validator = new \Rakit\Validation\Validator([
+    'required' => 'Поле :attribute обязательно для заполнения',
+    'numeric' => 'Значение поля :attribute должно быть числовым',
+  ]);
+
+  $validation = $validator->make($_POST, [
+    'name'                  => 'required',
+    'price'                 => 'required|numeric',
+    'description'           => 'required',
+    'details'               => 'required',
+  ]);
+
+  $validation->validate();
+
+  if ($validation->fails()) {
+    echo json_encode(['error' => $validation->errors()->firstOfAll()]);
+    exit();
+  } else {
+    echo json_encode($productDetail->create($_POST["name"], $_POST["price"], $_POST["description"], $_POST["details"]));
+  }
   exit();
 }
